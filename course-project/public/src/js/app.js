33a9be3fb78fedac62eea29d085b7db78b2af8ea
Jakob Leer <jpleer@tdcadsl.dk>
2018-10-12 15:15:10 +0200

    Creating subscriptions in front and store in back

--- course-project/public/src/js/app.js
+++ course-project/public/src/js/app.js
@@ -63,13 +63,58 @@ function askForNotificationPermission() {
   Notification.requestPermission(function(choice) {
     console.log('User chose: ', choice);
     if (choice === 'granted') {
-      displayConfirmNotificationFromSW();
+      configurePushSubscription();
+      // displayConfirmNotificationFromSW();
     }
   });
 }
 
+var reg;
+
+function configurePushSubscription() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  navigator.serviceWorker.ready
+    .then(swReg => {
+      reg = swReg;
+      return swReg.pushManager.getSubscription();
+    })
+    .then(sub => {
+      if (sub === null) {
+        // Create new subscription
+        const vapidPublicKey = 'BJSWji6qiyMrBB2usbw7uVOa0sxI71jPzljxyQSPPAAPQEa7wNqYYktrg4c-YLvT7FoXvmtkikPdMz2vyjqb_KE';
+        const convertedKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedKey
+        });
+      } else {
+        // Use existing subscription
+      }
+    })
+    // Store new subscription in Firebase database
+    .then(newSub => {
+      return fetch('https://pwagram-jakob-leer.firebaseio.com/subscriptions.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(newSub)
+      })
+    })
+    .then(res => {
+      if (res.ok) {
+        displayConfirmNotificationFromSW();
+      }
+    })
+    .catch(err => console.log(err));
+}
+
 // Notifications supported
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
   enableNotificationsButtons.forEach(function(btn) {
     btn.style.display = 'inline-block';
     btn.addEventListener('click', askForNotificationPermission);
