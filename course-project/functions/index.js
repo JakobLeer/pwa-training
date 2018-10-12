2e38055a91c0a6be49d8b54f5a754dc0050ed028
Jakob Leer <jpleer@tdcadsl.dk>
2018-10-12 12:38:47 +0200

    Implemented storePosts function

--- course-project/functions/index.js
+++ course-project/functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = require('./pwagram-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://pwagram-jakob-leer.firebaseio.com/'
+});
+
+exports.storePosts = functions.https.onRequest((request, response) => {
+ cors(request, response, function() {
+   admin.database().ref('posts').push(request.body)
+    .then(function() {
+      response.status(201).json({message: 'Data stored', id: request.body.id});
+    })
+    .catch(function(err) {
+      response.status(500).json({error: err});
+    });
+ });
+});
