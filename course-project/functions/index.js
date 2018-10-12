35f5485be09bd178b0334f00a3b6b8ca775ca681
Jakob Leer <jpleer@tdcadsl.dk>
2018-10-12 20:59:17 +0200

    Back-end pushes notifications

--- course-project/functions/index.js
+++ course-project/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
+const webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -16,6 +17,27 @@ exports.storePosts = functions.https.onRequest((request, response) => {
  cors(request, response, function() {
    admin.database().ref('posts').push(request.body)
     .then(function() {
+      webpush.setVapidDetails('mailto:jpleer@tdcadsl.dk',
+          'BJSWji6qiyMrBB2usbw7uVOa0sxI71jPzljxyQSPPAAPQEa7wNqYYktrg4c-YLvT7FoXvmtkikPdMz2vyjqb_KE',
+          '5J9mxQgukpDoNKhloc9zedHXHr6HLZqPB-9C8CR04uw'
+      );
+      return admin.database().ref('subscription').once('value');
+    })
+    .then(subscriptions => {
+      subscriptions.forEach(sub => {
+        var pushConfig = {
+          endpoint: sub.val().endpoint,
+          keys: {
+            auth: sub.val().keys.auth,
+            p256dh: sub.val().keys.p256dh
+          }
+        };
+        webpush.sendNotification(pushConfig. JSON.stringify({
+          title: 'New Post',
+          content: 'New Post added!'
+        }))
+          .catch(err => console.log(err));
+      });
       response.status(201).json({message: 'Data stored', id: request.body.id});
     })
     .catch(function(err) {
